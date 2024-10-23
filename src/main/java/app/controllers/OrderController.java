d6ef7463d929fec94744540254a04fab86f079fb
Falcho <m.k.falcham@gmail.com>
2024-10-23 13:42:20 +0200

    Added the getUserBasket method that lets us see the items added to users basket and calculates the price of all added items

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,10 +1,39 @@
 package app.controllers;
 
+import app.persistence.OrderMapper;
+import app.entities.ProductLine;
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+import io.javalin.*;
+
+import io.javalin.http.Context;
+
+import java.util.List;
+
+
 public class OrderController {
-    public static void addRoutes(){
+
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.get("/basketpage", ctx -> getUserBasket(ctx, connectionPool));
     }
 
-    public static void createOrder(){
+    public static void getUserBasket(Context ctx, ConnectionPool connectionPool) {
+        int userId = ctx.sessionAttribute("currentUser");
+        try {
+            List<ProductLine> basket = OrderMapper.getUserBasket(userId, connectionPool);
+            double totalPrice = basket.stream()
+                                        .mapToDouble(pl -> pl.getCupcake().getPrice() * pl.getQuantity())
+                                        .sum();
+            ctx.attribute("basket", basket);
+            ctx.attribute("totalPrice", totalPrice);
+            ctx.render("basketPage.html");
+        } catch (DatabaseException e) {
+            ctx.attribute("message", "Something went wrong, Try again");
+            ctx.render("basketPage.html");
+        }
+    }
+
+    public static void createOrder (){
     }
 
     public static void deleteOrder(){
