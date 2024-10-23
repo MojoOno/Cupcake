81873eb64b068afdb853d8d0fe8e8f72c4b60ee2
MojoOno <Ffranck0406@gmail.com>
2024-10-23 13:45:18 +0200

    Imported relevant stuff Added code to createUser

--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -1,8 +1,39 @@
 package app.persistence;
 
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserMapper {
 
-    public static void createUser(String username, String password) {
+    public static void createUser(String userName, String password, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "insert into users (user_name, user_password) values (?,?)";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        )
+        {
+            ps.setString(1, userName);
+            ps.setString(2, password);
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1)
+            {
+                throw new DatabaseException("Fejl ved oprettelse af ny bruger");
+            }
+        }
+        catch (SQLException e)
+        {
+            String msg = "Der er sket en fejl. Prøv igen";
+            if (e.getMessage().startsWith("ERROR: duplicate key value "))
+            {
+                msg = "Brugernavnet findes allerede. Vælg et andet";
+            }
+            throw new DatabaseException(msg, e.getMessage());
+        }
     }
 
     public static void getAllUsers() {
@@ -10,5 +41,4 @@ public class UserMapper {
 
     public static void login(String username, String password) {
     }
-
 }
