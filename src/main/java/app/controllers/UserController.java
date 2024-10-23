e12c6ae23eae1bf4e59430809b6a9d68a4b9da79
MojoOno <Ffranck0406@gmail.com>
2024-10-23 13:44:26 +0200

    Imported relevant stuff. Added routes to the addRoutes method Added code to createUser

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,16 +1,43 @@
 package app.controllers;
 
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+import app.persistence.UserMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
 public class UserController {
 
-    public static void addRoutes(){
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.get("/createuser", ctx -> ctx.render("createuser.html"));
+        app.post("/createuser", ctx -> createUser(ctx, connectionPool));
     }
 
-    public static void login(){
+    public static void login() {
     }
 
-    public static void createUser(){
+    private static void createUser(Context ctx, ConnectionPool connectionPool) {
+        // Hent form parametre
+        String username = ctx.formParam("username");
+        String password1 = ctx.formParam("password1");
+        String password2 = ctx.formParam("password2");
+
+        if (password1.equals(password2)) {
+            try {
+                UserMapper.createUser(username, password1, connectionPool);
+                ctx.attribute("message", "Du er hermed oprettet med brugernavn: " + username +
+                        ". Nu skal du logge på.");
+                ctx.render("index.html");
+            } catch (DatabaseException e) {
+                ctx.attribute("message", "Dit brugernavn findes allerede. Prøv igen, eller log ind");
+                ctx.render("createuser.html");
+            }
+        } else {
+            ctx.attribute("message", "Dine to passwords matcher ikke! Prøv igen");
+            ctx.render("createuser.html");
+        }
     }
 
-    public static void getAllUsers(){
+    public static void getAllUsers() {
     }
 }
