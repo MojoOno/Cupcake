2b42582b7cdbdf886ea03ecf11804747419a4d87
Falcho <m.k.falcham@gmail.com>
2024-10-23 11:53:39 +0200

    updated method to be more in line with what we want.

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -1,6 +1,11 @@
 package app.persistence;
 
+import app.Main;
+import app.entities.Bottom;
+import app.entities.Cupcake;
 import app.entities.ProductLine;
+import app.entities.Topping;
+import app.exceptions.DatabaseException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,7 +14,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static app.Main.connectionPool;
+
+
 public class OrderMapper {
+
 
     public static void createOrder(int userId, int orderTotal) {
     }
@@ -31,28 +40,27 @@ public class OrderMapper {
 
     public static List<ProductLine> getUserBasket(int userId) {
         List<ProductLine> productLines = new ArrayList<>();
-        String query = "SELECT pl.productline_id, t.topping_id, t.price AS topping_price, b.bottom_id, b.price AS bottom_price\n" +
-                "FROM productline pl\n" +
-                "JOIN orders o ON pl.order_id = o.order_id\n" +
-                "JOIN topping t ON pl.topping_id = t.topping_id\n" +
-                "JOIN bottom b ON pl.bottom_id = b.bottom_id\n" +
+        String sql = "SELECT pl.productline_id, t.topping_id, t.topping_price AS topping_price, b.bottom_id, b.bottom_price AS bottom_price " +
+                "FROM productline pl " +
+                "JOIN orders o ON pl.order_id = o.order_id " +
+                "JOIN topping t ON pl.topping_id = t.topping_id " +
+                "JOIN bottom b ON pl.bottom_id = b.bottom_id " +
                 "WHERE o.user_id = ?";
 
-        try (Connection conn = Database.getConnection();
-             PreparedStatement ps = conn.prepareStatement(query)) {
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+            ) {
             ps.setInt(1, userId);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                ProductLine productLine = new ProductLine(
-                        rs.getInt("productline_id"),
-                        rs.getInt("topping_id"),
-                        rs.getDouble("topping_price"),
-                        rs.getInt("bottom_id"),
-                        rs.getDouble("bottom_price")
-                );
+                Topping topping = new Topping(rs.getInt("topping_id"), rs.getFloat("topping_price"));
+                Bottom bottom = new Bottom(rs.getInt("bottom_id"), rs.getFloat("bottom_price"));
+                Cupcake cupcake = new Cupcake(bottom, topping);
+                ProductLine productLine = new ProductLine(rs.getInt("productline_id"), cupcake);
                 productLines.add(productLine);
             }
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
