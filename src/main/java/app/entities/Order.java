18994e45c4eebd65a02ca419da3d2370874dae4b
Falcho <m.k.falcham@gmail.com>
2024-10-24 11:07:39 +0200

    updated class with attribute more fitting for our needs, added constructor and getters

--- src/main/java/app/entities/Order.java
+++ src/main/java/app/entities/Order.java
@@ -3,10 +3,27 @@ package app.entities;
 import java.util.List;
 
 public class Order {
-    int orderId;
-    User user;
-    List<ProductLine> productList;
-    float orderPrice;
-    boolean isPaid;
+    private int orderId;
+    private int userId;
+    private List<ProductLine> productList;
+    private Double orderPrice;
+    private boolean isPaid;
 
+    public Order(int orderId, int userId, Double orderPrice) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.orderPrice = orderPrice;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public Double getOrderPrice() {
+        return orderPrice;
+    }
 }
